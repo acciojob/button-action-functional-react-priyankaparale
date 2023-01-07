4086f47f43e7f765bee9b0be0248753fdc685f73
Priyanka Parale <priyankaparale8@gmail.com>
2023-01-07 15:22:15 +0530

    used conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [display, setdisplay] = useState(false);
+  
+  
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={setdisplay(true)}> </button>
+      {(display===false)?(null) :(<p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>) }
     </div>
   );
 }
